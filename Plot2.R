a7e80f0091f2096727538d117f930c1bc618cfe7
Mitsuokara <35892747+Mitsuokara@users.noreply.github.com>
2018-04-15 19:13:04 -0300

    Final

--- Plot2.R
+++ Plot2.R
@@ -5,7 +5,7 @@ dados$Time<-x
 dados$Date<-as.Date(dados$Date, format="%d/%m/%Y")
 result<-subset(dados, Date %in% c(as.Date("2007/02/01"), as.Date("2007/02/02")))
 result$Global_active_power <- sapply(result$Global_active_power, as.numeric)
-hist(result$Global_active_power, col = "red", main = "Global Active Power",xlab = "Global Active Power (kilowatts)")
-dev.copy(png, "plot1.png")
+plot(result$Time, result$Global_active_power, type = "l", ylab = "Global Active Power (kilowatts)", xlab="")
+dev.copy(png, "plot2.png")
 dev.off()
-     
+  

f9137f45d75e565474f6a59f7015b10ab0b94d09
Mitsuokara <35892747+Mitsuokara@users.noreply.github.com>
2018-04-14 21:25:56 -0300

    Fix_Plot1

--- Plot1.R
+++ Plot1.R
@@ -1,5 +1,6 @@
-dados<-read.csv("household_power_consumption.txt",header = TRUE, sep = ";", colClasses = c("Date", rep("character",8)))
-result<-subset(dados, Date %in% c(as.Date("01/02/2017"), as.Date("02/02/2017")))
+dados<-read.csv("household_power_consumption.txt",header = TRUE, sep = ";", colClasses = c(rep("character",9)), na.strings = "?")
+dados$Date<-as.Date(dados$Date, format="%d/%m/%Y")
+result<-subset(dados, Date %in% c(as.Date("2007/02/01"), as.Date("2007/02/02")))
 result$Global_active_power <- sapply(result$Global_active_power, as.numeric)
 hist(result$Global_active_power, col = "red", main = "Global Active Power",xlab = "Global Active Power (kilowatts)")
 dev.copy(png, "plot1.png")
